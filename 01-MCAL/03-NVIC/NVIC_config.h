d3138aa87eb9ffb3b27d2a784531a324f45e1335
Mustafa Ali <mustafaali4813@gmail.com>
2023-07-21 22:24:13 +0300

    nvic

--- 01-MCAL/03-NVIC/NVIC_config.h
+++ 01-MCAL/03-NVIC/NVIC_config.h
@@ -4,6 +4,13 @@
 /******************************/
 #ifndef NVIC_CONFIG_H
 #define NVIC_CONFIG_H
-
+/* choose:
+NVIC_GROUB4_SUB0
+NVIC_GROUB3_SUB1
+NVIC_GROUB2_SUB2
+NVIC_GROUB1_SUB3
+NVIC_GROUB0_SUB4
+*/
+#define NVIC_GROUP_SUB_ID   NVIC_GROUB4_SUB0
 
 #endif
